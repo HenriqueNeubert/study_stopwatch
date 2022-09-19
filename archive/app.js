186568712c4ad271b9a183ad7bc6f578c0d7f33a
henriqueneubert <neubert.study@gmail.com>
2022-09-19 10:04:42 -0300

    adicionado comando de inicializacao do projeto em package.json 0.5

--- archive/app.js
+++ archive/app.js
@@ -1,20 +1,96 @@
-const inputName = document.getElementById('name')
-const inputStart = document.getElementById('initial')
-const inputStop = document.getElementById('stop') 
-const inputResume = document.getElementById('resume') 
-const inputNewTime = document.getElementById('newTime') 
+const form = document.getElementById('form') 
+
+const timer = document.getElementById('timer') 
 const inputNext = document.getElementById('next') 
+const inputName = document.getElementById('name')
+const inputStop = document.getElementById('stop') 
 const inputReset = document.getElementById('reset') 
 const inputSubmit = document.getElementById('submit') 
+const inputResume = document.getElementById('resume') 
+const inputStart = document.getElementById('initial')
+const inputNewTime = document.getElementById('newTime') 
 
 const boxInputName = document.getElementById('boxInputName') 
 
 let cron
-let timeHour = document.getElementById('hour') 
-let timeMinute = document.getElementById('minute') 
-let timeSecond = document.getElementById('second') 
-let timeMillisecond = document.getElementById('millisecond') 
+const timeHourElem = document.getElementById('hour') 
+const timeMinuteElem = document.getElementById('minute')
+const timeSecondElem = document.getElementById('second') 
+const timeMillisecondElem = document.getElementById('millisecond') 
 
+let timeHour = 0
+let timeMinute = 0
+let timeSecond = 0
+let timeMillisecond = 0
+
+hide(timer)
+hide(inputStop)
+hide(inputStop)
+hide(inputStart)
+hide(inputReset)
+hide(inputSubmit)
+hide(inputResume)
+hide(inputNewTime)
+
+function HandleSubmit(event)
+{
+  event.preventDefault();
+  const data = handleData();  
+  handleInsertDadaBase(data);  
+  hide(inputReset)
+  hide(inputSubmit)
+  hide(inputResume)
+  show(inputNewTime)
+}
+
+function handleInsertDadaBase(data)
+{
+  const arr = handleGetDataBase();
+  arr.push(data);
+
+  const newData = JSON.stringify(arr);
+  localStorage.setItem('Atletas', newData)  
+}
+
+function handleGetDataBase()
+{
+  const data = localStorage.getItem('Atletas');
+  if(data){
+    return JSON.parse(data);
+  }
+
+  return []
+} 
+
+function handleData()
+{
+  const objItem = new Object();
+  objItem.name = saveDataName();
+  objItem.time = saveDataTime();
+  
+  return objItem
+}
+
+//*SAVE DATA
+function saveDataTime()
+{    
+  return{
+    horas: timeHourElem.innerText,  
+    minutos: timeMinuteElem.innerText,
+    segundos: timeSecondElem.innerText,
+    millisegundos: timeMillisecondElem.innerText,
+  }  
+}
+
+function saveDataName()
+{  
+  let nameData = inputName.value  
+  document.getElementById('showName').innerText = nameData //!provisorio
+  return nameData
+}
+//*SAVE DATA
+
+//* FOR TIMER --- START
 function handleTimer()
 {
     if ((timeMillisecond += 10) == 1000) {
@@ -29,38 +105,42 @@ function handleTimer()
       timeMinute = 0;
       timeHour++;
     }
-    document.getElementById('hour').innerText = returnData(timeHour);
-    document.getElementById('minute').innerText = returnData(timeMinute);
-    document.getElementById('second').innerText = returnData(timeSecond);
-    document.getElementById('millisecond').innerText = returnData(timeMillisecond);
+    timeHourElem.innerText = returnData(timeHour);
+    timeMinuteElem.innerText = returnData(timeMinute);
+    timeSecondElem.innerText = returnData(timeSecond);
+    timeMillisecondElem.innerText = returnData(timeMillisecond);
   }
   
   function returnData(input) {
     return input >= 10 ? input : `0${input}`
 }
+//* FOR TIMER --- END
 
-function handleBottons($el)
-{
-  if($el === 'next'){
-    if(validation() === true){      
-      saveDataName() 
-      none(inputNext)
-      none(boxInputName)
-      flex(inputSubmit)
-      flex(inputStop)
-      flex(inputStart)            
-    }      
-  }
+//* FOR BOTTONS --- START
+function handleNext()
+{     
+  if(validation() === true){  
+    saveDataName() 
+    hide(boxInputName)
+    hide(inputNext)
+    handleResetTime()     
+    show(timer)
+    show(inputStart)      
+  }  
 }
 
-function handleReload(el)
+function handleNewTime()
 {
-  el.preventDefault()
+  
 }
 
 function handleStart()
 {
-  handlePause();
+  show(reset)
+  show(inputStop)
+  hide(inputStart)
+  // handlePause();///!IMPORTANTE
+  hide(inputResume)
   //? setInterval = a cada 10 milissegundos(
   //? pois a cada 1 milissegundo trava dependendo do navegador).
   cron = setInterval(() => { handleTimer(); }, 10)
@@ -68,41 +148,44 @@ function handleStart()
 
 function handlePause()
 {
+  hide(inputStop)
+  show(inputResume)
+  show(inputSubmit)
   //? para não termos vários timers funcionando ao fundo
   clearInterval(cron); 
 }
 
 function handleResetTime()
 {  
+  show(inputStart)
+  hide(inputResume)
+
   timeHour = 0;
   timeMinute = 0;
   timeSecond = 0;
   timeMillisecond = 0;
-  document.getElementById('hour').innerHTML = '00';
-  document.getElementById('minute').innerHTML = '00';
-  document.getElementById('second').innerHTML = '00';
-  document.getElementById('millisecond').innerHTML = '000';
-}
 
-//*SAVE DATA
-function saveDataName()
-{  
-  let nameData = inputName.value
-  
+  timeHourElem.innerHTML = '00';
+  timeMinuteElem.innerHTML = '00';
+  timeSecondElem.innerHTML = '00';
+  timeMillisecondElem.innerHTML = '000';
+
+  handlePause()
+  hide(submit)
+  hide(inputStop)
+  hide(inputReset)
+  hide(inputResume)
 }
-//*SAVE DATA
+//* FOR BOTTONS --- END
 
 //* SHOW OR NO
-function flex(el)
+function hide(el)
 {
-  el.style.display = 'none';    
-  return false   
+  el.style.display = 'none';   
 }
-
-function none(el)
+function show(el)
 {
-  el.style.display = 'none';    
-  return false   
+  el.style.display = 'flex';  
 }
 //* SHOW OR NO
 
@@ -118,10 +201,10 @@ function validation()
 }
 //*VALIDACOES
 
-inputStart.addEventListener('click', handleStart)
-inputNext.addEventListener('click', function(){
-  handleBottons('next')
-})
-// inputNext.addEventListener('submit', handleReload, false)
+inputNext.addEventListener('click', handleNext)
 inputStop.addEventListener('click', handlePause)
+inputStart.addEventListener('click', handleStart)
+inputResume.addEventListener('click', handleStart)
+form.addEventListener('submit', HandleSubmit, false)
+inputNewTime.addEventListener('click', handleNewTime)
 inputReset.addEventListener('click', handleResetTime)
